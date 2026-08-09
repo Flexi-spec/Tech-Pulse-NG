4825fc224627e362def53dec2cf9bd9cade5d5f5
Flexi-spec <devflexi20@gmail.com>
2026-08-09 04:44:18 +0100

    Update Supabase URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.TP_CONFIG = {
-  SUPABASE_URL: "https://YOUR-PROJECT.supabase.co",
+  SUPABASE_URL: "https://rkgfahkzhzohbfpvvbyp.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_QZBztpkjq4f4aBofUemwxw_TyU4yLZw"
 };
